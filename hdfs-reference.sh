a9b948b18b78cd400fc70c8159cead00e0558795
zhikuan-wang <zhikuan.wang@outlook.com>
2017-08-18 18:14:31 +0800

    Update hdfs-reference.sh

--- hdfs-reference.sh
+++ hdfs-reference.sh
@@ -7,3 +7,14 @@ hdfs dfs -setfacl -m -R default:other::r-x /cdsh
 hdfs dfs -setfacl -b -R /cdsh
 # Get ACL
 hdfs dfs -getfacl /cdsh
+
+# Resolve the umask and ACL conflict
+# 1, change umask to 002 and restart cluster.
+hive-site.xml
+<name>fs.permissions.umask-mode</name>
+<value>006</value>
+# 2, Change ACL
+hdfs dfs -setfacl -m -R default:user:hive:rwx /cdsh
+hdfs dfs -setfacl -m -R default:other::r-x /cdsh
+hdfs dfs -setfacl -m -R user:hive:rwx /cdsh
+hdfs dfs -chmod -R 775 /cdsh
