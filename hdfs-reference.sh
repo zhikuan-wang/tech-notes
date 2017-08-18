47709ce47be9a388ef8d27c0898247c497ad67f7
zhikuan-wang <zhikuan.wang@outlook.com>
2017-08-18 12:08:50 +0800

    Update hdfs-reference.sh

--- hdfs-reference.sh
+++ hdfs-reference.sh
@@ -1,5 +1,8 @@
 # Set ACL
 hdfs dfs -setfacl -m -R user:hive:rwx /cdsh
+# Set default ACL
+hdfs dfs -setfacl -m -R default:user:hive:rwx /cdsh
+hdfs dfs -setfacl -m -R default:other::r-x /cdsh
 # Clear ACL
 hdfs dfs -setfacl -b -R /cdsh
 # Get ACL
