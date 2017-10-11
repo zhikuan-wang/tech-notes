25f35bae78bb322bb90fa16c7b7b0f5c1dce910a
Wang, Steven Zhikuan <zhikuan.wang@outlook.com>
2017-10-11 10:50:15 +0800

    Update rhel-cmd-reference.sh

--- rhel-cmd-reference.sh
+++ rhel-cmd-reference.sh
@@ -1,2 +1,6 @@
 # Check the signature of installed rpm packages
 rpm -qa --qf '%{NAME}-%{VERSION}-%{RELEASE}-%{ARCH} %{SIGPGP:pgpsig} %{SIGGPG:pgpsig}\n' | grep -i mysql-community-common
+
+# Online extend
+lvextend -L +100G /dev/mapper/rootvg-appsvol
+resize2fs -p /dev/mapper/rootvg-appsvol
