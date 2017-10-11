a1f21c4bd09517e51c0bfb4b5e3c2fa1a1858234
Wang, Steven Zhikuan <zhikuan.wang@outlook.com>
2017-10-11 17:47:34 +0800

    Update rhel-cmd-reference.sh

--- rhel-cmd-reference.sh
+++ rhel-cmd-reference.sh
@@ -4,3 +4,9 @@ rpm -qa --qf '%{NAME}-%{VERSION}-%{RELEASE}-%{ARCH} %{SIGPGP:pgpsig} %{SIGGPG:pg
 # Online extend
 lvextend -L +100G /dev/mapper/rootvg-appsvol
 resize2fs -p /dev/mapper/rootvg-appsvol
+
+# Python HTTP File server
+# Python 2.7.x
+python -m SimpleHTTPServer
+# Python 3.x
+python -m  http.server 8000
