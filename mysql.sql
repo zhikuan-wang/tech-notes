0382ddab9d9784c6888148f311aed7e4cc0febda
Wang, Steven Zhikuan <zhikuan.wang@outlook.com>
2017-10-22 19:39:26 +0800

    Update mysql.sql

--- mysql.sql
+++ mysql.sql
@@ -1,3 +1,12 @@
 create user 'docker'@'%' identified by 'VTPhz8Gj';
 grant ALL on docker.* to 'docker'@'%';
 flush privileges;
+
+CREATE TABLE `docker`.`book` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `author` VARCHAR(255) NOT NULL,
+  `title` VARCHAR(255) NOT NULL,
+  `publish_date` DATETIME NULL,
+  PRIMARY KEY (`ID`)
+) COMMENT="book";
+insert into book(`author`, `title`, `publish_date`) values ('Steven', 'Hello Docker', NOW());
