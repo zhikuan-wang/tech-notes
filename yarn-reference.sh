c33663ca5cc2fdc919046e5a4e9f2c0bc34c5581
Wang, Steven Zhikuan <zhikuan.wang@outlook.com>
2017-12-01 16:51:15 +0800

    Update yarn-reference.sh

--- yarn-reference.sh
+++ yarn-reference.sh
@@ -2,3 +2,5 @@
 yarn application -list
 # Get the application ID from Application Attempt Id
 yarn applicationattempt -status <Application Attempt Id>
+# Get the application log
+yarn logs -applicationId <application ID>
